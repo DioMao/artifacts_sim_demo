ddf86d4f45b747a24c8f474b730087f86d05008f
JuanWeiMao <hmh593588523@outlook.com>
2021-09-09 12:13:23 +0800

    demo_update

--- components/artifact-upgrade.js
+++ components/artifact-upgrade.js
@@ -15,17 +15,53 @@ app.component("artifact-upgrade",{
         </button>
         <div class="entryBox">
             <div class="mb-3">+{{ Artifact.level }}</div>
-            <div class="mb-3" style="font-size: 1.2rem"> {{ toChinese(Artifact.mainEntry,"mainEntry") }} <span class="float-end"> {{ mainEntryValue(Artifact.mainEntry,Artifact.mainEntryValue) }} </span></div>
-            <div v-for="entry in Artifact.entry">
-                <div class="mb-1">{{ toChinese(entry[0],"entry") }} <span class="float-end">{{ showEntryList(entry[0],entry[1]) }}</span></div>
+            <div class="mb-1 mainEntry"><span class="me-1" style="color:rgb(172,160,148);">✦</span> {{ toChinese(Artifact.mainEntry,"mainEntry") }} <span class="float-end"> {{ mainEntryValue(Artifact.mainEntry,Artifact.mainEntryValue) }} </span></div>
+            <div class="entryLine"></div>
+            <div class="entryList mb-1" v-for="entry in Artifact.entry">
+                <div class=""><span class="me-1" style="color:rgb(172,160,148);">•</span> {{ toChinese(entry[0],"entry") }} <span class="float-end">{{ showEntryList(entry[0],entry[1]) }}</span></div>
             </div>
         </div>
-        <button @click="upgrade" class="btn btn-genshin upgrade-button-lg"><span class="circleinbox"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;强化&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+        <button @click="upgrade" class="btn btn-genshin upgrade-button-lg" v-show="Artifact.level<20"><span class="circleinbox"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;强化&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+        <div class="upgradeMax" v-show="Artifact.level>=20">已达到当前等级上限</div>
+        <div class="myMask" v-show="showUpdate">
+            <div class="upgradeAlert ani-AlertBoxUp">
+                <div class="UpAlertHead">
+                    <div class="upgradeImgBox">
+                        <div class="upgradeImg">
+                            <img :src="'img/A-'+Artifact.part+'.png'" :alt="Artifact.part">
+                        </div>
+                        <div class="UpLevelStar">
+                            <span v-for="i in 5" style="margin-right: 2px;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFCC32" class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="upLevel">
+                            +{{ Artifact.level }}
+                        </div>
+                    </div>
+                    <div class="upgradeMsg">升级</div>
+                </div>
+                <div class="UpAlertBody ani-showAlertBody">
+                    <div class="entryBox UpAlertEntry">
+                        <div class="mb-1 mainEntry"><span class="me-1" style="color:rgb(172,160,148);">✦</span> {{ toChinese(Artifact.mainEntry,"mainEntry") }} <span class="float-end"> {{ mainEntryValue(Artifact.mainEntry,Artifact.mainEntryValue) }} </span></div>
+                        <div class="entryList mb-1" v-for="entry in Artifact.entry">
+                            <div class=""><span class="me-1" style="color:rgb(172,160,148);">•</span> {{ toChinese(entry[0],"entry") }} <span class="float-end">{{ showEntryList(entry[0],entry[1]) }}</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="upAlertBtn ani-AlertBtnShow">
+                <button class="btn btn-genshin" @click="showUpdate=false"><span class="circleinbox"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;确认&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+            </div>
+        </div>
     </div>
     `,
     props: ["index"],
     data(){
         return {
+            showUpdate: false,                  // 显示升级结果
             ArtifactsList: [],
             Artifact: {
                 level: 0,
@@ -78,7 +114,7 @@ app.component("artifact-upgrade",{
             this.localRecord(this.ArtifactsList);
             this.Artifact = JSON.parse(JSON.stringify(ArtifactsSim.result[this.index]));
             if(res == true){
-                this.alertControl("升级成功！",1500);
+                this.showUpdate = true;
             }else{
                 this.alertControl("当前圣遗物已满级~",1500,"warning");
             }
